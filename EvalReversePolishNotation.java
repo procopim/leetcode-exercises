dd0e6fdcde2e7ecc6140f859b117fce4a5871ce8
Mark Procopio <77703070+procopim@users.noreply.github.com>
2023-09-08 01:22:08 +0000

    EvaluateReversePolishNotation.java

---
+++ EvalReversePolishNotation.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int evalRPN(String[] tokens) {
+        int[] stack = new int[tokens.length];
+        int top = -1;
+        for(String token : tokens){
+            if (isOperator(token)){
+                if (top >= 1){
+                    int b = stack[top--];
+                    int a = stack[top--];
+                    top++;
+                    if (token.equals("+")){
+                        stack[top] = a+b;
+                    }
+                    else if (token.equals("-")){
+                        stack[top] = a-b;
+                    }
+                    else if (token.equals("*")){
+                        stack[top] = a*b;
+                    }
+                    else if (token.equals("/")){
+                        stack[top] = a/b;
+                    }
+                }
+                else{
+                    return 0;
+                }
+            } else{
+                top++;
+                stack[top] = Integer.valueOf(token);
+            }
+        }
+        return stack[top];
+    }
+    private boolean isOperator(String token){
+        return token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/");
+    }
+}
+//https://leetcode.com/problems/evaluate-reverse-polish-notation/
